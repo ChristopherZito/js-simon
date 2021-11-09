99cffe392f453275b7b0958892e2339f20ece042
christopherzito <christopherzito11@gmail.com>
2021-11-09 14:55:49 +0100

    update finale

--- js/script.js
+++ js/script.js
@@ -3,10 +3,47 @@
 // Dopo 30 secondi l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 
+//const id pagina
+const numPag = document.getElementById("num-gen");
+const cont = document.getElementById("container");
+
+
 //generaione 5 numeri non ripeturi
+const numeri = [];
+while(numeri.length < 5){
+    const numGen = Math.floor(Math.random() * 100) + 1;
+
+    let verifica = numeri.includes(numGen);
+    if (verifica == false){
+        numeri.push(numGen)
+    }    
+}
+numPag.innerHTML += numeri;
+console.log("i numeri generati sono: ",numeri);
+
+setTimeout(() => {
+    cont.style.display = "none"
+}, 800)
 
 //delay di 30 secondi
-//apparizione promt
-
-//confronto dei numeri inseriti e quelli generati
-//risultato 
+setTimeout(() => {
+    
+    const numUser = [];
+    //inserimento dei numeri da parte di user
+    numeri.forEach((userIn) => {
+        //apparizione promt
+        userIn = parseInt(prompt("inserire uno dei 5 numeri mostrati"));
+        numUser.push(userIn)
+    });
+    console.log("l'array di numeri dell'utente è: ",numUser);
+    //confronto dei numeri inseriti e quelli generati
+    const risult = numeri.filter((base) => {
+        if(numUser.includes(base)){
+            return base;
+        }
+    });
+    //risultato 
+    cont.style.display = "block";
+    numPag.innerHTML = risult ,",";
+    console.log("i numeri che combaciano sono: ",risult);
+}, 1000)
